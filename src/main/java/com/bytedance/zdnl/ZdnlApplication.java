9a31289670040602e9bdc24bee3b914c1da87f3e
yanzhenxing <zhenxing.yan@duobei.com>
2021-11-02 19:51:29 +0800

    init

---
+++ src/main/java/com/bytedance/zdnl/ZdnlApplication.java
@@ -0,0 +1,13 @@
+package com.bytedance.zdnl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdnlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZdnlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bytedance/zdnl/ZdnlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytedance.zdnl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZdnlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
