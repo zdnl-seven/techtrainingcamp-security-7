5f9c5a776e1ab86e183e059950777bd50376bc3a
yanzhenxing <zhenxing.yan@duobei.com>
2021-11-06 14:48:45 +0800

    修改配置文件

--- src/test/java/com/example/zdnl7/Zdnl7ApplicationTests.java
+++ src/test/java/com/example/zdnl7/Zdnl7ApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.zdnl7;
 
+import com.example.zdnl7.Entity.RequestInfo;
+import com.example.zdnl7.Entity.UserInfo;
+import com.example.zdnl7.Entity.VerifyCodeInfo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
 class Zdnl7ApplicationTests {
+    @Autowired
+    RequestInfo requestInfo;
+
+    @Autowired
+    UserInfo userInfo;
+
+    @Autowired
+    VerifyCodeInfo verifyCodeInfo;
+
+
 
     @Test
-    void contextLoads() {
+    void generateTables(){
+
 
     }
 
