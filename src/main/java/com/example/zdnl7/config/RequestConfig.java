57d189db3d6a4cdfc69ed5f9a327f83f2074fa92
angrychow <angrychow@qq.com>
2021-11-07 19:33:24 +0800

    新增一个Interceptors用于检查当前ip是否被登记

---
+++ src/main/java/com/example/zdnl7/config/RequestConfig.java
@@ -0,0 +1,23 @@
+package com.example.zdnl7.config;
+
+import com.example.zdnl7.config.interceptors.ReqInterceptors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class RequestConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ReqInterceptors reqInterceptors;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(reqInterceptors).addPathPatterns("/**");
+    }
+
+
+}
+
---
+++ src/main/java/com/example/zdnl7/config/interceptors/ReqInterceptors.java
@@ -0,0 +1,39 @@
+package com.example.zdnl7.config.interceptors;
+
+import com.example.zdnl7.dao.RequestDao;
+import com.example.zdnl7.entity.RequestInfo;
+import com.example.zdnl7.utils.ConstUtil;
+import org.checkerframework.common.aliasing.qual.LeakedToResult;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+@Component
+public class ReqInterceptors implements HandlerInterceptor {
+
+    @Resource
+    RequestDao reqInfo;
+
+    @Resource
+    ConstUtil constUtil;
+
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response){
+        if(reqInfo.existsByIp(request.getParameter(constUtil.PARAM_KEY_IP))==false) {
+
+            reqInfo.save(new RequestInfo(
+                    Long.parseLong(request.getParameter(constUtil.PARAM_KEY_SESSION_ID)),
+                    new Date().getTime(),
+                    request.getParameter(constUtil.PARAM_KEY_IP),
+                    request.getParameter(constUtil.PARAM_KEY_DEVICE_ID),
+                    new Date()
+            ));
+        }
+
+        return true;
+    }
+}
